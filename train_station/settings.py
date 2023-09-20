21e080d419d519e490cfefca3b38d5c2ca24c249
Olena Liubynetska <olena.liuby@gmail.com>
2023-09-20 18:46:28 +0100

    refactor: Use environment variable for Django SECRET_KEY

--- train_station/settings.py
+++ train_station/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wg@tl+_*bi2ez!xu(2ns8*)3z+axjl-!hqkbqrw65dum*l&4y*"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
